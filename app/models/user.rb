29fd5219ca5b3751cde7cb1a899ea05aba47301c
ArunPrasath <arun.prasath@artha42.com>
2010-09-22 01:07:40 +0530

    session timeout for application

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   acts_as_authentic do |c|
     c.validate_email_field = false
+    c.logged_in_timeout = 5.minutes
   end
   
   has_many :products, :through => :accessibles
